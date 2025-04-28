c40407009c93594fb30f01b417dc7fd9b4e83f05
Hyunsoo KIM <howeve18@gmail.com>
2025-04-28 20:37:19 +0900

    add: 로그인 페이지 구성 및 가짜 api 적용 완료

---
+++ rebridge-fe/rebridge/lib/shared/styles/logo_styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class LogoStyles {
+  static const double width = 200;
+  static const double height = 200;
+  static const BoxFit fit = BoxFit.contain;
+}

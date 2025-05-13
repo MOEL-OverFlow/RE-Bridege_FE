724ca8287cf0302054082014e39e31d3257f11ac
Hyunsoo KIM <howeve18@gmail.com>
2025-05-13 17:36:25 +0900

    Feat : My Page 수정 API 연결 및 뒤로 가기 실행 시 fetchRandom 리렌더링 완료

--- rebridge-fe/rebridge/lib/core/router/router.dart
+++ rebridge-fe/rebridge/lib/core/router/router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:rebridge/main.dart';
 import 'package:rebridge/pages/agree_terms_page.dart';
 import 'package:rebridge/pages/checklist_page.dart';
 import 'package:rebridge/pages/company_list_page.dart';
@@ -63,5 +64,6 @@ final appRouterProvider = Provider<GoRouter>((ref) {
           path: '/companylists',
           builder: (context, state) => const CompanyListPage())
     ],
+    observers: [routeObserver],
   );
 });
